19a08b144647bce18e5325b1c2f45eb7e9c0424f
liikii <609957891@qq.com>
2018-04-03 17:01:05 +0800

    up3

--- smpweb.py
+++ smpweb.py
@@ -1,5 +1,59 @@
-from tornado import web, gen, ioloop
 from json import loads
+import logging
+
+from tornado.websocket import websocket_connect
+from tornado import web, gen, ioloop, websocket
+
+
+logger = logging.getLogger('simple_task')
+logger.setLevel(logging.DEBUG)
+
+fh = logging.FileHandler('simple_task.log')
+fh.setLevel(logging.DEBUG)
+
+fmt = '%(asctime)s - %(name)s - %(levelname)s \n%(message)s\n'
+formatter = logging.Formatter(fmt)
+fh.setFormatter(formatter)
+
+logger.addHandler(fh)
+
+
+class WebSocketHandler(websocket.WebSocketHandler):
+    clt_s = set()
+    # csz = 108
+    msg = []
+
+    @classmethod
+    def send_updates(cls, chat):
+        for clt in cls.clt_s:
+            try:
+                clt.write_message(chat)
+            except:
+                logger.error("Error sending message", exc_info=True)
+
+    @gen.coroutine
+    def open(self):
+        x_real_ip = self.request.headers.get("X-Real-IP")
+        remote_ip = x_real_ip or self.request.remote_ip
+        logger.info('log in %s ' % remote_ip)
+        WebSocketHandler.clt_s.add(self)
+        if len(WebSocketHandler.clt_s) > 200:
+            WebSocketHandler.clt_s.pop()
+        for m in WebSocketHandler.msg:
+            self.write_message(m)
+
+    @gen.coroutine
+    def on_message(self, message):
+        print('ws', message)
+        logger.info('task : %s ' % message)
+        WebSocketHandler.send_updates(message)
+        WebSocketHandler.msg.append(message)
+        if len(WebSocketHandler.msg) > 200:
+            WebSocketHandler.msg.pop(0)
+
+    def on_close(self):
+        print('close')
+        WebSocketHandler.clt_s.add(self)
 
 
 class IdxHandler(web.RequestHandler):
@@ -17,7 +71,7 @@ class IdxHandler(web.RequestHandler):
         return
 
 
-class MHandler(web.RequestHandler):
+class MainHandler(web.RequestHandler):
     @gen.coroutine
     def get(self):
         return
@@ -25,17 +79,22 @@ class MHandler(web.RequestHandler):
     @gen.coroutine
     def post(self):
         bdy = self.request.body
-        dt = loads(bdy.decode('utf-8'))
+        ubd = bdy.decode('utf-8')
+        dt = loads(ubd)
         self.set_status(404, 'ddd')
         self.write('{}')
+
+        ws = yield websocket_connect('ws://127.0.0.1:8666/ws')
+        ws.write_message(ubd)
         return
 
 
 def make_app():
     return web.Application([
-        (r'/favicon.ico', MHandler),
-        (r'/haha', MHandler),
+        (r'/favicon.ico', MainHandler),
+        (r'/haha', MainHandler),
         (r'/?', IdxHandler),
+        (r'/ws', WebSocketHandler),
         (r'/s/(.*)', web.StaticFileHandler, {"path": './static'})
     ], autoreload=True)
 
